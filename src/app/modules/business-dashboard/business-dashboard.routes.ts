46487fd3b69865ae848ef1b3e54fb05fc6f9345b
damith <damith.dev@gmail.com>
2021-12-08 01:28:04 +0530

    route fix

--- src/app/modules/business-dashboard/business-dashboard.routes.ts
+++ src/app/modules/business-dashboard/business-dashboard.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from 'src/app/auth/auth.guard';
 import { BusinessDashbaordComponent } from './business-dashbaord/business-dashbaord.component';
 
-export const BusinessDashboardRoutes: Routes = [{ path: '', component: BusinessDashbaordComponent,canActivate: [AuthGuard],
+export const BusinessDashboardRoutes: Routes = [{ path: 'home', component: BusinessDashbaordComponent,canActivate: [AuthGuard],
 }]
